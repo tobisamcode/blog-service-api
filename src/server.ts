2872165b768f3307114bcf789e0dfc97d6fee5a6
tobisamcode <samueloluwatobiloba48@gmail.com>
2023-01-07 13:54:58 +0100

    first restful api with typescript

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import compression from 'compression';
+import cors from 'cors';
+import morgan from 'morgan';
+import Controller from '@/utils/interfaces/controller.interface';
+import ErrorMiddleware from '@/middleware/error.middleware';
