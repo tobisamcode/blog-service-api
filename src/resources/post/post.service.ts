95f9170bb4bb8f1fa74790a9b0cf3d76666dc09a
tobisamcode <samueloluwatobiloba48@gmail.com>
2023-01-09 00:04:02 +0100

    protecting my routes

--- src/resources/post/post.service.ts
+++ src/resources/post/post.service.ts
@@ -4,6 +4,26 @@ import Post from '@/resources/post/post.interface';
 class PostService {
     private post = PostModel;
 
+    public async get(): Promise<any> {
+        try {
+            const posts = await this.post.find({});
+
+            return posts;
+        } catch (error) {
+            throw new Error('Unable to get posts');
+        }
+    }
+
+    public async getById(id: string): Promise<any> {
+        try {
+            const post = await this.post.findById(id);
+
+            return post;
+        } catch (error) {
+            throw new Error('Unable to get post');
+        }
+    }
+
     // create a new post
     public async create(
         title: string,
@@ -15,6 +35,29 @@ class PostService {
             return post;
         } catch (error) {
             throw new Error('Unable to create post');
+        }
+    }
+
+    public async update(
+        id: string,
+        body: {}
+    ): Promise<Post | undefined | null> {
+        try {
+            await this.post.findByIdAndUpdate({ _id: id }, { ...body });
+            const updatedPost = await this.post.findById(id);
+            return updatedPost;
+        } catch (error) {
+            throw new Error('Unable to update post');
+        }
+    }
+
+    public async delete(id: string): Promise<any> {
+        try {
+            const post = await this.post.findByIdAndDelete({ _id: id });
+
+            return post;
+        } catch (error) {
+            throw new Error('Unable to delete post');
         }
     }
 }
