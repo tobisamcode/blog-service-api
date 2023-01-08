0dcc924aa0ca5243cade0b4f617c44d815449c18
tobisamcode <samueloluwatobiloba48@gmail.com>
2023-01-09 00:03:16 +0100

    authentication in typescript express

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,12 @@ import App from './server';
 
 import validateEnv from '@/utils/validateEnv';
 import PostController from '@/resources/post/post.controller';
-import GetController from '@/resources/get/get.controller';
-import GetByIdController from '@/resources/getById/getbyid.controller';
-import DeleteController from '@/resources/delete/delete.controller';
-import UpdateController from '@/resources/update/update.controller';
 import UserController from '@/resources/user/user.controller';
 
 validateEnv();
 
 const app = new App(
-    [
-        new PostController(),
-        new GetController(),
-        new GetByIdController(),
-        new DeleteController(),
-        new UpdateController(),
-        new UserController(),
-    ],
+    [new PostController(), new UserController()],
     Number(process.env.PORT)
 );
 
